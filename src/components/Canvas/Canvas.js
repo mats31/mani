d0bcd6075aa0cd83ab1fa29b19ed666db64a2d50
Mathis <mathis.biabiany@gmail.com>
2016-10-20 00:54:41 +0200

    First commit

--- src/components/Canvas/Canvas.js
+++ src/components/Canvas/Canvas.js
@@ -26,6 +26,7 @@ export default Vue.extend({
       shortVideos: [],
 
       maskWidth: 0,
+      maskHeight: 0,
     };
   },
 
@@ -71,7 +72,8 @@ export default Vue.extend({
 
       this.currentVideo = this.shortVideos[0].media;
       this.currentVideo.play();
-      this.currentVideo = this.shortVideos[1].media;
+      this.nextVideo = this.shortVideos[1].media;
+      this.nextVideo.play();
     },
 
     animate() {
@@ -79,22 +81,42 @@ export default Vue.extend({
 
       const canvas = this.canvas;
       const currentVideo = this.currentVideo;
+      const nextVideo = this.nextVideo;
+
+      this.ctx.save();
 
       this.drawMask( canvas );
 
-    //   this.ctx.drawImage( currentVideo, 0, 0, currentVideo.videoWidth, currentVideo.videoHeight, 0, 0, canvas.width, canvas.height );
+      this.ctx.globalCompositeOperation = 'source-in';
+
+      this.ctx.drawImage( nextVideo, 0, 0, nextVideo.videoWidth, nextVideo.videoHeight, 0, 0, canvas.width, canvas.height );
+
+      this.ctx.restore();
     },
 
     drawMask( canvas ) {
       const x = canvas.width * 0.41640625;
+      const x2 = canvas.width * 0.58333333333333;
+      const x3 = x;
       const y = canvas.height;
-      const width = this.maskWidth = ( this.maskWidth + ( ( this.maskWidth - x ) * 0.1 ) ) * -1;
+      const y2 = y;
+      const y3 = 0;
+      let width = this.maskWidth = this.maskWidth + ( ( x - this.maskWidth ) * 0.1 );
+      const width2 = width;
+      width *= -1;
+      const width3 = x2 - x;
       const height = canvas.height * -1;
+      const height2 = height;
+      const height3 = this.maskHeight = this.maskHeight + ( ( canvas.height - this.maskHeight ) * 0.1 );
 
-    //   console.log(width);
-
-      this.ctx.fillStyle = 'white';
+      this.ctx.fillStyle = 'red';
       this.ctx.fillRect( x, y, width, height );
+
+      this.ctx.fillStyle = 'red';
+      this.ctx.fillRect( x2, y2, width2, height2 );
+
+      this.ctx.fillStyle = 'red';
+      this.ctx.fillRect( x3, y3, width3, height3 );
     },
 
   },
